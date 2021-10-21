8b596d3d05cec8dd7953f5013aac77967237dffd
Udi Finkelstein <github@udifink.com>
2021-10-21 15:14:11 +0300

    Added "not" operator for squery.

--- squery.py
+++ squery.py
@@ -8,22 +8,26 @@ import sys
 
 or_keywords = ('"or"i', '"או"')
 and_keywords = ('"and"i', '"וגם"')
+not_keywords = ('"not"i', '"ללא"')
 
 or_kw_str = ""
 and_kw_str = ""
+not_kw_str = ""
 squery_parser = None
 
 #
 # Call this if you want to localize your and/or keywords
 #
-def squery_compile_parser(or_kw_str=or_kw_str, and_kw_str=and_kw_str):
+def squery_compile_parser(or_kw_str=or_kw_str, and_kw_str=and_kw_str, not_kw_str=not_kw_str):
     global squery_parser
     parser_ebnf = r"""
         ?q_or : q_and (ws ( {or_kw} )  ws q_and)*
         ?q_and: q_val (ws ( {and_kw} ) ws q_val)*
         ?q_val: escaped_string
               | string
+              | ( {not_kw} ) q_not 
               | "(" q_or ")"
+        q_not: q_val
         string : STRING
         escaped_string : ESCAPED_STRING
         ws : WS
@@ -32,19 +36,20 @@ def squery_compile_parser(or_kw_str=or_kw_str, and_kw_str=and_kw_str):
         %import common.WS
         %ignore WS
 
-    """.format(or_kw=or_kw_str, and_kw=and_kw_str)
+    """.format(or_kw=or_kw_str, and_kw=and_kw_str, not_kw=not_kw_str)
     #print(parser_ebnf)
     squery_parser = Lark(parser_ebnf, start='q_or')
 
 # if you want to translate and/or to a different language
-def squery_set_keywords(or_kw, and_kw):
+def squery_set_keywords(or_kw, and_kw, not_kw):
     global or_kw_str, and_kw_str
     or_kw_str = ' | '.join(or_kw)
     and_kw_str = ' | '.join(and_kw)
-    squery_compile_parser(or_kw_str, and_kw_str)
+    not_kw_str = ' | '.join(not_kw)
+    squery_compile_parser(or_kw_str, and_kw_str, not_kw_str)
 
 # set default keywords
-squery_set_keywords(or_keywords, and_keywords)
+squery_set_keywords(or_keywords, and_keywords, not_keywords)
 
 #
 # Compile a query, return a query function that accepts a string and returns True or False
@@ -81,6 +86,11 @@ def outer_s_or(*f):
         return False
     return s_or
 
+def outer_s_not(func):
+    def s_not(text):
+        return not func(text)
+    return s_not
+
 class SQuery_transformer(Transformer):
     def q_or(self, items):
         #print("q_or:", items, list(filter(None, items)))
@@ -88,6 +98,9 @@ class SQuery_transformer(Transformer):
     def q_and(self, items):
         #print("q_and:", items, list(filter(None, items)))
         return outer_s_and(*list(filter(None, items)))
+    def q_not(self, items):
+        #print("q_not:", items[0])
+        return outer_s_not(items[0])
     def escaped_string(self, items):
         #print("string:", items)
         return outer_s_string(items[0][1:-1])
@@ -109,7 +122,8 @@ if __name__ == "__main__":
             sys.exit(0)
         print("Enter query:")
         s = input()
-        #print(squery_parser.parse(s).pretty())
+        print(squery_parser.parse(s).pretty())
         query = squery_compile(s)
+        print(query)
         print(query(text))
 
